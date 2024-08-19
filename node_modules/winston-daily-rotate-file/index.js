9a98efa9240bbf6a7ec8cb68e43e3d57338d7c66
Fredrik Burmester <fredrik.burmester@gmail.com>
2024-08-19 08:20:56 +0200

    fix

--- node_modules/winston-daily-rotate-file/index.js
+++
@@ -1,5 +0,0 @@
-const winston = require("winston");
-const DailyRotateFile = require("./daily-rotate-file");
-
-winston.transports.DailyRotateFile = DailyRotateFile;
-module.exports = DailyRotateFile;
